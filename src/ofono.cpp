fd888db50a9b82b05643ee13cb5f590b9c5547fe
Marek Belisko <marek.belisko@open-nandra.com>
2012-04-28 16:58:15 +0200

    Add handling for incoming messages to ofono class.
    Signed-off-by: Marek Belisko <marek.belisko@open-nandra.com>

--- src/ofono.cpp
+++ src/ofono.cpp
@@ -9,14 +9,26 @@ Ofono::Ofono(QObject *parent)
 	connect(m_ofonoManager, SIGNAL(modemRemoved(QString)), this, SLOT(on_ofonoManager_modemremoved(QString)));
 
 	qDebug() << "Available modems:" << m_ofonoManager->modems();
+
 }
 
 void Ofono::on_ofonoManager_modemAdded(const QString &path)
 {
 	qDebug() << "Modem added:" << path;
+
+	m_ofonoMessageManager = new OfonoMessageManager(OfonoModem::AutomaticSelect, path);
+	connect(m_ofonoMessageManager, SIGNAL(incomingMessage(QString,QVariantMap)), this, SLOT(on_ofonoMessageManager_incomingMessage(QString,QVariantMap)));
 }
 
 void Ofono::on_ofonoManager_modemremoved(const QString &path)
 {
 	qDebug() << "Modem removed:" << path;
+
+	disconnect(m_ofonoMessageManager, SIGNAL(incomingMessage(QString,QVariantMap)), 0, 0);
+	delete m_ofonoMessageManager;
+}
+
+void Ofono::on_ofonoMessageManager_incomingMessage(const QString& message, const QVariantMap& info)
+{
+	qDebug() << "Message retrieved:" << message << "Info:" << info;
 }
--- src/ofono.h
+++ src/ofono.h
@@ -3,6 +3,7 @@
 
 #include <QObject>
 #include "ofonomodemmanager.h"
+#include "ofonomessagemanager.h"
 
 class Ofono : public QObject
 {
@@ -12,10 +13,12 @@ public:
 
 private:
 	OfonoModemManager *m_ofonoManager;
+	OfonoMessageManager *m_ofonoMessageManager;
 
 private slots:
 	void on_ofonoManager_modemAdded(const QString&);
 	void on_ofonoManager_modemremoved(const QString&);
+	void on_ofonoMessageManager_incomingMessage(const QString& message, const QVariantMap& info);
 };
 
 #endif // OFONO_H
