84fca76d70ceb591b6f5ed1981932f15f1fd553d
Marek Belisko <marek.belisko@open-nandra.com>
2012-05-07 23:14:42 +0200

    ofono: Add modem enabling after startup.
    Signed-off-by: Marek Belisko <marek.belisko@open-nandra.com>

--- src/ofono.cpp
+++ src/ofono.cpp
@@ -1,15 +1,18 @@
 #include "ofono.h"
 #include <QDebug>
+#include <QStringList>
 
 Ofono::Ofono(QObject *parent)
 	: QObject(parent)
+    , m_modemInit(false)
 {
 	m_ofonoManager = new OfonoModemManager();
 	connect(m_ofonoManager, SIGNAL(modemAdded(QString)), this, SLOT(on_ofonoManager_modemAdded(QString)));
 	connect(m_ofonoManager, SIGNAL(modemRemoved(QString)), this, SLOT(on_ofonoManager_modemremoved(QString)));
 
 	qDebug() << "Available modems:" << m_ofonoManager->modems();
-
+	if (!m_ofonoManager->modems().isEmpty())
+		on_ofonoManager_modemAdded(m_ofonoManager->modems().at(0));
 }
 
 void Ofono::on_ofonoManager_modemAdded(const QString &path)
@@ -18,6 +21,15 @@ void Ofono::on_ofonoManager_modemAdded(const QString &path)
 
 	m_ofonoMessageManager = new OfonoMessageManager(OfonoModem::AutomaticSelect, path);
 	connect(m_ofonoMessageManager, SIGNAL(incomingMessage(QString,QVariantMap)), this, SLOT(on_ofonoMessageManager_incomingMessage(QString,QVariantMap)));
+
+	/* Enable modem by default */
+	m_ofonoModem = new OfonoModem(OfonoModem::ManualSelect, path);
+    connect(m_ofonoModem, SIGNAL(poweredChanged(bool)), this, SLOT(on_ofonoModem_poweredChanged(bool)));
+    connect(m_ofonoModem, SIGNAL(onlineChanged(bool)), this, SLOT(on_ofonoModem_onlineChanged(bool)));
+
+    if (!m_ofonoModem->powered()) {
+		m_ofonoModem->setPowered(true);
+    }
 }
 
 void Ofono::on_ofonoManager_modemremoved(const QString &path)
@@ -26,9 +38,35 @@ void Ofono::on_ofonoManager_modemremoved(const QString &path)
 
 	disconnect(m_ofonoMessageManager, SIGNAL(incomingMessage(QString,QVariantMap)), 0, 0);
 	delete m_ofonoMessageManager;
+
+	delete m_ofonoModem;
+    m_modemInit = false;
 }
 
 void Ofono::on_ofonoMessageManager_incomingMessage(const QString& message, const QVariantMap& info)
 {
 	qDebug() << "Message retrieved:" << message << "Info:" << info;
+}
+
+void Ofono::on_ofonoModem_poweredChanged(bool powered)
+{
+    if (!powered) {
+        qDebug() << __func__ << "powered failed";
+        return;
+    }
+
+    if (!m_ofonoModem->online())
+        m_ofonoModem->setOnline(true);
+}
+
+void Ofono::on_ofonoModem_onlineChanged(bool online)
+{
+    if (!online) {
+        qDebug() << __func__ << "online failed";
+        return;
+    }
+
+    qDebug() << "Modem powered & online.";
+
+    m_modemInit = true;
 }
--- src/ofono.h
+++ src/ofono.h
@@ -4,6 +4,7 @@
 #include <QObject>
 #include "ofonomodemmanager.h"
 #include "ofonomessagemanager.h"
+#include "ofonomodem.h"
 
 class Ofono : public QObject
 {
@@ -14,11 +15,15 @@ public:
 private:
 	OfonoModemManager *m_ofonoManager;
 	OfonoMessageManager *m_ofonoMessageManager;
+	OfonoModem *m_ofonoModem;
+    bool m_modemInit;
 
 private slots:
 	void on_ofonoManager_modemAdded(const QString&);
 	void on_ofonoManager_modemremoved(const QString&);
 	void on_ofonoMessageManager_incomingMessage(const QString& message, const QVariantMap& info);
+    void on_ofonoModem_poweredChanged(bool powered);
+    void on_ofonoModem_onlineChanged(bool online);
 };
 
 #endif // OFONO_H
