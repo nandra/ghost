6519355f803634b52d62c7dad01a0711bb6b58f7
Marek Belisko <marek.belisko@open-nandra.com>
2012-05-10 22:54:07 +0200

    Add simple logging to file.
    Signed-off-by: Marek Belisko <marek.belisko@open-nandra.com>

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,45 @@
 #include "ofono.h"
 #include "webserver.h"
 #include "database.h"
+#include <fstream>
+#include <QTime>
+
+using namespace std;
+ofstream logfile;
+
+void SimpleLoggingHandler(QtMsgType type, const char *msg)
+{
+    switch (type) {
+    case QtDebugMsg:
+        logfile << QTime::currentTime().toString().toAscii().data() << " Debug: " << msg << "\n";
+        fprintf(stderr, "%s [DEBUG]: %s\n", QTime::currentTime().toString().toAscii().data(), msg);
+        break;
+    case QtCriticalMsg:
+        logfile << QTime::currentTime().toString().toAscii().data() << " Critical: " << msg << "\n";
+        fprintf(stderr, "%s [CRITICAL]: %s\n", QTime::currentTime().toString().toAscii().data(), msg);
+        break;
+    case QtWarningMsg:
+        logfile << QTime::currentTime().toString().toAscii().data() << " Warning: " << msg << "\n";
+        fprintf(stderr, "%s [WARN]: %s\n", QTime::currentTime().toString().toAscii().data(), msg);
+        break;
+    case QtFatalMsg:
+        logfile << QTime::currentTime().toString().toAscii().data() <<  " Fatal: " << msg << "\n";
+        fprintf(stderr, "%s [FATAL]: %s\n", QTime::currentTime().toString().toAscii().data(), msg);
+        abort();
+    }
+
+    logfile.flush();
+ }
 
 int main(int argc, char *argv[])
 {
+    if (argc > 1) {
+        // argument 1 is logfile for logging
+        logfile.open(argv[1], ios::app);
+    }
+
+    qInstallMsgHandler(SimpleLoggingHandler);
+
     QApplication a(argc, argv);
 
 	new Database();
